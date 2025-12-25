42f5b50f897ae6b1523d095c95c60a6bea63dd1d
acbc <acbc.dev@gmail.com>
2025-12-25 12:53:58 -0500

    feat: add new sidebar item and sub-item components, and enhance textarea styling with an error variant.

---
+++ src/features/ui/sidebar-item/index.ts
@@ -0,0 +1,7 @@
+import SidebarItem, { sidebarItem } from './SidebarItem.astro';
+import SidebarSubItem, { sidebarSubItem } from './SidebarSubItem.astro';
+
+const SidebarItemVariants = { sidebarItem };
+const SidebarSubItemVariants = { sidebarSubItem };
+
+export { SidebarItem, SidebarItemVariants, SidebarSubItem, SidebarSubItemVariants };
