e973cc35d89400b2c574e414c1529094f100687b
acbc <acbc.dev@gmail.com>
2025-12-25 12:53:25 -0500

    feat: introduce Radio and Sidebar navigation components, and enhance Input and Textarea with error states and styling.

--- src/features/ui/input/index.ts
+++ src/features/ui/input/index.ts
@@ -1,7 +1,9 @@
 import Input, { input } from "./Input.astro";
+import SearchInput, { searchInput, searchInputWrapper, searchInputIcon } from "./SearchInput.astro";
+import InputError, { inputError } from "./InputError.astro";
 
-const InputVariants = { input };
+const InputVariants = { input, searchInput, searchInputWrapper, searchInputIcon, inputError };
 
-export { Input, InputVariants };
+export { Input, InputVariants, SearchInput, InputError };
 
 export default Input;
