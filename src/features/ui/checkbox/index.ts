6d9dc9a664713fb38ff922a298f79058cddcc0fd
acbc <acbc.dev@gmail.com>
2025-12-25 12:52:56 -0500

    feat: introduce new Radio, Checkbox, Sidebar, InputError, and SearchInput components and update styling for Textarea and Dialog components.

---
+++ src/features/ui/checkbox/index.ts
@@ -0,0 +1,7 @@
+import Checkbox, { checkbox, checkboxWrapper, checkboxLabel, checkboxIcon } from "./Checkbox.astro";
+
+const CheckboxVariants = { checkbox, checkboxWrapper, checkboxLabel, checkboxIcon };
+
+export { Checkbox, CheckboxVariants };
+
+export default Checkbox;
